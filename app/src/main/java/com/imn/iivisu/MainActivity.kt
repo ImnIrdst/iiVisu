05c798208e6895174a9e926dbada0e00a983103a
iman irandoost <imn.irdst@gmail.com>
2020-10-30 20:29:14 +0330

    Add wav recorder

--- app/src/main/java/com/imn/iivisu/MainActivity.kt
+++ app/src/main/java/com/imn/iivisu/MainActivity.kt
@@ -2,18 +2,46 @@ package com.imn.iivisu
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.squti.androidwaverecorder.WaveRecorder
 import com.imn.iivisu.databinding.ActivityMainBinding
-import com.imn.iivisu.utils.showToast
+import com.imn.iivisu.utils.checkAudioPermission
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private var isRecording = false
+
+    private lateinit var f: File
+    private lateinit var r: WaveRecorder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.recordButton.setOnClickListener { showToast("Hello World") }
+        checkAudioPermission(AUDIO_PERMISSION_REQUEST_CODE)
+
+        f = File(filesDir, "rec.wav")
+        r = WaveRecorder(f.toString())
+
+        binding.recordButton.setOnClickListener { toggleRecording() }
+    }
+
+    private fun toggleRecording() {
+        if (!isRecording) {
+            r.startRecording()
+            binding.recordButton.text = getString(R.string.stop)
+            isRecording = true
+        } else {
+            r.stopRecording()
+            binding.recordButton.text = getString(R.string.record)
+            isRecording = false
+        }
+    }
+
+    companion object {
+        private const val AUDIO_PERMISSION_REQUEST_CODE = 1
     }
 }
---
+++ app/src/main/java/com/imn/iivisu/utils/PermissionUtils.kt
@@ -0,0 +1,27 @@
+package com.imn.iivisu.utils
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+
+fun Activity.checkAudioPermission(requestCode: Int): Boolean {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (!isGranted(Manifest.permission.RECORD_AUDIO)) {
+            requestPermissions(
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                requestCode
+            )
+            return false
+        }
+    }
+    return true
+}
+
+fun Context.isGranted(permissionCode: String) =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        checkSelfPermission(permissionCode) == PackageManager.PERMISSION_GRANTED
+    } else {
+        true
+    }
